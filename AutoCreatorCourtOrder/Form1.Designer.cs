4525ced6fd7244b562c8c8ec45081c3b19e6fe1c
dimas6000 <diman6000@yandex.ru>
2018-11-11 22:17:01 +0700

    Добавлена загрузка .rft в richTextBox

--- AutoCreatorCourtOrder/Form1.Designer.cs
+++ AutoCreatorCourtOrder/Form1.Designer.cs
@@ -28,13 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.openFileButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.DetectUrls = false;
+            this.richTextBox1.Location = new System.Drawing.Point(13, 13);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(1200, 600);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // openFileButton
+            // 
+            this.openFileButton.Location = new System.Drawing.Point(12, 632);
+            this.openFileButton.Name = "openFileButton";
+            this.openFileButton.Size = new System.Drawing.Size(137, 48);
+            this.openFileButton.TabIndex = 1;
+            this.openFileButton.Text = "Открыть файл";
+            this.openFileButton.UseVisualStyleBackColor = true;
+            this.openFileButton.Click += new System.EventHandler(this.openFileButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(1224, 692);
+            this.Controls.Add(this.openFileButton);
+            this.Controls.Add(this.richTextBox1);
+            this.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button openFileButton;
     }
 }
 
--- AutoCreatorCourtOrder/Form1.cs
+++ AutoCreatorCourtOrder/Form1.cs
@@ -16,5 +16,35 @@ namespace AutoCreatorCourtOrder
         {
             InitializeComponent();
         }
+
+        private void openFileButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //чужой код без изменений во всём try
+                using (OpenFileDialog dialog = new OpenFileDialog())
+                {
+                    dialog.CheckFileExists = true;
+                    dialog.CheckPathExists = true;
+                    dialog.Multiselect = false;
+                    dialog.Title = "Выберите файл";
+                    dialog.Filter = "rtf files (*.rtf)|*.rtf";
+                    if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                    {
+                        string filename = dialog.FileName;
+                        this.richTextBox1.LoadFile(filename);
+                    }
+                }
+            }
+            catch (System.IO.IOException)
+            {
+                MessageBox.Show("Выбранный файл не может быть открыт, возможно он используется другой программой.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Произошла неизвестная ошибка при считывании файла/n" + ex.ToString());
+            }
+        }
+
     }
 }
