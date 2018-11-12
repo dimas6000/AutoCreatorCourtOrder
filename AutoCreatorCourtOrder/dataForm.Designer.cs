8a352e907d0cceeb02762a4860291aa28453831f
dimas6000 <diman6000@yandex.ru>
2018-11-12 13:32:47 +0700

    Новая форма для отображения извлеченных данных

---
+++ AutoCreatorCourtOrder/dataForm.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace AutoCreatorCourtOrder
+{
+    partial class dataForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fullNameTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.dobTextBox = new System.Windows.Forms.TextBox();
+            this.innTextBox = new System.Windows.Forms.TextBox();
+            this.allDebtTextBox = new System.Windows.Forms.TextBox();
+            this.bplTextBox = new System.Windows.Forms.TextBox();
+            this.addressTextBox = new System.Windows.Forms.TextBox();
+            this.debtStructureListBox = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // fullNameTextBox
+            // 
+            this.fullNameTextBox.Location = new System.Drawing.Point(118, 12);
+            this.fullNameTextBox.Name = "fullNameTextBox";
+            this.fullNameTextBox.Size = new System.Drawing.Size(1094, 27);
+            this.fullNameTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Полное имя";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Дата рождения";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(137, 19);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Место рождения";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 19);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Адрес";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(475, 114);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 19);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "ИНН";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(520, 168);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(143, 19);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Структура долга:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(855, 114);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(110, 19);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Сумма долга";
+            // 
+            // dobTextBox
+            // 
+            this.dobTextBox.Location = new System.Drawing.Point(145, 111);
+            this.dobTextBox.Name = "dobTextBox";
+            this.dobTextBox.Size = new System.Drawing.Size(199, 27);
+            this.dobTextBox.TabIndex = 8;
+            // 
+            // innTextBox
+            // 
+            this.innTextBox.Location = new System.Drawing.Point(524, 111);
+            this.innTextBox.Name = "innTextBox";
+            this.innTextBox.Size = new System.Drawing.Size(218, 27);
+            this.innTextBox.TabIndex = 9;
+            // 
+            // allDebtTextBox
+            // 
+            this.allDebtTextBox.Location = new System.Drawing.Point(971, 111);
+            this.allDebtTextBox.Name = "allDebtTextBox";
+            this.allDebtTextBox.Size = new System.Drawing.Size(241, 27);
+            this.allDebtTextBox.TabIndex = 10;
+            // 
+            // bplTextBox
+            // 
+            this.bplTextBox.Location = new System.Drawing.Point(155, 78);
+            this.bplTextBox.Name = "bplTextBox";
+            this.bplTextBox.Size = new System.Drawing.Size(1057, 27);
+            this.bplTextBox.TabIndex = 11;
+            // 
+            // addressTextBox
+            // 
+            this.addressTextBox.Location = new System.Drawing.Point(75, 45);
+            this.addressTextBox.Name = "addressTextBox";
+            this.addressTextBox.Size = new System.Drawing.Size(1137, 27);
+            this.addressTextBox.TabIndex = 12;
+            // 
+            // debtStructureListBox
+            // 
+            this.debtStructureListBox.FormattingEnabled = true;
+            this.debtStructureListBox.ItemHeight = 19;
+            this.debtStructureListBox.Location = new System.Drawing.Point(16, 190);
+            this.debtStructureListBox.Name = "debtStructureListBox";
+            this.debtStructureListBox.Size = new System.Drawing.Size(1196, 422);
+            this.debtStructureListBox.TabIndex = 13;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(30, 641);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1224, 692);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.debtStructureListBox);
+            this.Controls.Add(this.addressTextBox);
+            this.Controls.Add(this.bplTextBox);
+            this.Controls.Add(this.allDebtTextBox);
+            this.Controls.Add(this.innTextBox);
+            this.Controls.Add(this.dobTextBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.fullNameTextBox);
+            this.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Name = "dataForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "dataForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox fullNameTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox dobTextBox;
+        private System.Windows.Forms.TextBox innTextBox;
+        private System.Windows.Forms.TextBox allDebtTextBox;
+        private System.Windows.Forms.TextBox bplTextBox;
+        private System.Windows.Forms.TextBox addressTextBox;
+        private System.Windows.Forms.ListBox debtStructureListBox;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ AutoCreatorCourtOrder/dataForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoCreatorCourtOrder
+{
+    public partial class dataForm : Form
+    {
+        public dataForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
