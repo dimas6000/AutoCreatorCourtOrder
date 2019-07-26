d55a5e627360cd33705a7b154a344fde224e3493
dimas6000 <diman6000@yandex.ru>
2019-07-26 19:51:17 +0700

    Новый файл, хранит пути к файлам.

---
+++ AutoCreatorCourtOrder/PathsData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoCreatorCourtOrder
+{
+    /// <summary>
+    /// Хранит пути к файлам и папкам.
+    /// </summary>
+    class PathsData
+    {
+        /// <summary>
+        /// Путь к шаблону приказа.
+        /// </summary>
+        public static string PathToTemplate { get; set; }
+
+        /// <summary>
+        /// Путь к обрабатываемому файлу.
+        /// </summary>
+        public static string PathToFileBeingProcessed { get; set; }
+
+        /// <summary>
+        /// Путь к папке с приказами для обработки всех сразу.
+        /// </summary>
+        //  public static string PathToFolder { get; set; }
+
+    }
+}
