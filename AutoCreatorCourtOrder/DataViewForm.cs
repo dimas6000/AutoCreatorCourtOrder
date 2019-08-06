cd654f1519e7d3c25fb4a5622de667bce4f231ab
dimas6000 <diman6000@yandex.ru>
2019-08-07 00:09:05 +0700

    Класс ExtractedData теперь не статический, поэтому передаем его форме для отображения данных.

--- AutoCreatorCourtOrder/DataViewForm.cs
+++ AutoCreatorCourtOrder/DataViewForm.cs
@@ -13,23 +13,20 @@ namespace AutoCreatorCourtOrder
     // Теоретически в этой форме пользователь может просмотреть данные.Практически же она не нужна,
     // т.к. при обработке большого числа файлов каждый файл проверять никто не будет.
     // Так что я её только при отладке использую. Нужно переработать концепцию или удалить.
-
     public partial class DataViewForm : Form
     {
-        public DataViewForm()
+        public DataViewForm(ExtractedData extractedData)
         {
             InitializeComponent();
-            /*
-            fullNameTextBox.Text = ExtractedData.FullName;
-            addressTextBox.Text = ExtractedData.Address;
-            bplTextBox.Text = ExtractedData.BirthPlace;
-            dobTextBox.Text = ExtractedData.DateOfBirth;
-            innTextBox.Text = ExtractedData.Inn;
-            allDebtNumericUpDown.Value = Convert.ToDecimal(ExtractedData.AllDebt);
-            debtStructureRichTextBox.Text = ExtractedData.DebtStructure;
-            bankDetailsRichTextBox.Text = ExtractedData.BankDetails;
-            stateDutyNumericUpDown.Value = ExtractedData.CalculateStateDuty();
-            */
+            fullNameTextBox.Text = extractedData.FullName;
+            addressTextBox.Text = extractedData.Address;
+            bplTextBox.Text = extractedData.BirthPlace;
+            dobTextBox.Text = extractedData.DateOfBirth;
+            innTextBox.Text = extractedData.Inn;
+            allDebtNumericUpDown.Value = Convert.ToDecimal(extractedData.AllDebt);
+            debtStructureRichTextBox.Text = extractedData.DebtStructure;
+            bankDetailsRichTextBox.Text = extractedData.BankDetails;
+            stateDutyNumericUpDown.Value = extractedData.CalculateStateDuty();
         }
     }
 }
